ee2dfd40d312e375e3205d957d84f422c3f04e24
chavalit <chavalit.kow@gmail.com>
2020-11-13 17:39:27 +0700

    theme

---
+++ wp-content/themes/newsup/header.php
@@ -0,0 +1,248 @@
+<?php
+/**
+ * The header for our theme.
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @package Newsup
+ */
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?> >
+<?php wp_body_open(); ?>
+<div id="page" class="site">
+<a class="skip-link screen-reader-text" href="#content">
+<?php _e( 'Skip to content', 'newsup' ); ?></a>
+    <div class="wrapper" id="custom-background-css">
+        <header class="mg-headwidget">
+            <!--==================== TOP BAR ====================-->
+
+            <?php do_action('newsup_action_header_section');  ?>
+            <div class="clearfix"></div>
+            <?php $background_image = get_theme_support( 'custom-header', 'default-image' );
+            if ( has_header_image() ) {
+              $background_image = get_header_image();
+            } ?>
+            <div class="mg-nav-widget-area-back" style='background-image: url("<?php echo esc_url( $background_image ); ?>" );'>
+            <?php $remove_header_image_overlay = get_theme_mod('remove_header_image_overlay',false); ?>
+            <div class="overlay">
+              <div class="inner" <?php if($remove_header_image_overlay == false) { 
+            $newsup_header_overlay_color = get_theme_mod('newsup_header_overlay_color','rgba(32,47,91,0.4)');?> style="background-color:<?php echo esc_attr($newsup_header_overlay_color);?>;" <?php } ?>> 
+                <div class="container-fluid">
+                    <div class="mg-nav-widget-area">
+                        <div class="row align-items-center">
+                            <div class="col-md-12 col-sm-12 text-center-xs">
+                                <div class="navbar-header">
+                                <?php the_custom_logo(); 
+                                if (display_header_text()) : ?>
+                                <div class="site-branding-text">
+                                <h1 class="site-title"> <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
+                                <p class="site-description"><?php bloginfo('description'); ?></p>
+                                </div>
+                              <?php endif; ?>
+                                </div>
+                            </div>
+                           <?php do_action('newsup_action_banner_advertisement'); ?>
+
+                        </div>
+                    </div>
+                </div>
+              </div>
+              </div>
+          </div>
+    <div class="mg-menu-full">
+      <nav class="navbar navbar-expand-lg navbar-wp">
+        <div class="container-fluid flex-row-reverse">
+          <!-- Right nav -->
+                    <div class="m-header d-flex pl-3 ml-auto my-2 my-lg-0 position-relative align-items-center">
+                        <?php $home_url = home_url(); ?>
+                        <a class="mobilehomebtn" href="<?php echo esc_url($home_url); ?>"><span class="fa fa-home"></span></a>
+                        <!-- navbar-toggle -->
+                        <button class="navbar-toggler mx-auto" type="button" data-toggle="collapse" data-target="#navbar-wp" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                          <i class="fa fa-bars"></i>
+                        </button>
+                        <!-- /navbar-toggle -->
+                        <div class="dropdown show mg-search-box pr-2">
+                            <a class="dropdown-toggle msearch ml-auto" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                               <i class="fa fa-search"></i>
+                            </a>
+
+                            <div class="dropdown-menu searchinner" aria-labelledby="dropdownMenuLink">
+                        <?php get_search_form(); ?>
+                      </div>
+                        </div>
+                        
+                    </div>
+                    <!-- /Right nav -->
+         
+          
+                  <div class="collapse navbar-collapse" id="navbar-wp">
+                  	<div class="d-md-block">
+                  <?php wp_nav_menu( array(
+        								'theme_location' => 'primary',
+        								'container'  => 'nav-collapse collapse navbar-inverse-collapse',
+        								'menu_class' => 'nav navbar-nav mr-auto',
+        								'fallback_cb' => 'newsup_fallback_page_menu',
+        								'walker' => new newsup_nav_walker()
+        							) ); 
+        						?>
+        				</div>		
+              		</div>
+          </div>
+      </nav> <!-- /Navigation -->
+    </div>
+</header>
+<div class="clearfix"></div>
+<?php  if (is_front_page() || is_home()) { ?>
+<div class="p-5" style="background-image: url('http://demo.berthongpol.com/wp-content/uploads/2020/11/aVPexJ.jpg'); background-position: center; background-repeat: no-repeat; background-size: cover;">
+	<h4 class="mb-4 text-white">ทำนายเบอร์</h4>
+
+                       <form method="GET" action="https://explore.berthongpol.com/forecast" accept-charset="UTF-8" class="text-white" role="search">
+                            <div class="row ">
+                                <div class="form-group col-lg-6">
+                                    <label for="" class="text-white">กรุณาระบุเบอร์ที่ต้องการทำนาย (ตัวเลข 10 หลัก) <span class="text-danger">*</span></label>
+                                    <div class="input-group">
+                                        <input type="text" class="form-control" name="tel" placeholder="" value="0987654321" autocomplete="off" pattern="[0-9]{10}" required="">
+                                    </div>
+                                </div>
+                                <div class="form-group col-lg-4 d-none">
+                                    <label for="">วันที่เกิด (เดือน/วันที่/ปี)</label>
+                                    <div class="input-group">
+                                        <input type="date" class="form-control" name="date" placeholder="" value="<?php echo date('Y-m-d'); ?>" autocomplete="off">
+                                    </div>
+                                </div>
+                                <div class="form-group col-lg-2 d-none">
+                                    <label for="hour">เวลาเกิด (ชั่วโมง)</label>
+                                    <div class="input-group">
+                                    <select id="hour" name="hour" class="form-control">
+                                        <option value="">ไม่ระบุ</option>
+                                        <option value="00">00</option>
+                                        <option value="01">01</option>
+                                        <option value="02">02</option>
+                                        <option value="03">03</option>
+                                        <option value="04">04</option>
+                                        <option value="05">05</option>
+                                        <option value="06">06</option>
+                                        <option value="07">07</option>
+                                        <option value="08">08</option>
+                                        <option value="09">09</option>
+                                        <option value="10">10</option>
+                                        <option value="11">11</option>
+                                        <option value="12">12</option>
+                                        <option value="13">13</option>
+                                        <option value="14">14</option>
+                                        <option value="15">15</option>
+                                        <option value="16">16</option>
+                                        <option value="17">17</option>
+                                        <option value="18">18</option>
+                                        <option value="19">19</option>
+                                        <option value="20">20</option>
+                                        <option value="21">21</option>
+                                        <option value="22">22</option>
+                                        <option value="23">23</option>
+                                    </select>
+                                    <script>
+                                        document.querySelector("#hour").value = "";
+                                    </script>
+                                    </div>
+                                </div>
+                                <div class="form-group col-lg-2 d-none">
+                                    <label for="minute">เวลาเกิด (นาที)</label>
+                                    <div class="input-group">
+                                    <select id="minute" name="minute" class="form-control">
+                                        <option value="">ไม่ระบุ</option>
+                                        <option value="00">00</option>
+                                        <option value="01">01</option>
+                                        <option value="02">02</option>
+                                        <option value="03">03</option>
+                                        <option value="04">04</option>
+                                        <option value="05">05</option>
+                                        <option value="06">06</option>
+                                        <option value="07">07</option>
+                                        <option value="08">08</option>
+                                        <option value="09">09</option>
+                                        <option value="10">10</option>
+                                        <option value="11">11</option>
+                                        <option value="12">12</option>
+                                        <option value="13">13</option>
+                                        <option value="14">14</option>
+                                        <option value="15">15</option>
+                                        <option value="16">16</option>
+                                        <option value="17">17</option>
+                                        <option value="18">18</option>
+                                        <option value="19">19</option>
+                                        <option value="20">20</option>
+                                        <option value="21">21</option>
+                                        <option value="22">22</option>
+                                        <option value="23">23</option>
+                                        <option value="24">24</option>
+                                        <option value="25">25</option>
+                                        <option value="26">26</option>
+                                        <option value="27">27</option>
+                                        <option value="28">28</option>
+                                        <option value="29">29</option>
+                                        <option value="30">30</option>
+                                        <option value="31">31</option>
+                                        <option value="32">32</option>
+                                        <option value="33">33</option>
+                                        <option value="34">34</option>
+                                        <option value="35">35</option>
+                                        <option value="36">36</option>
+                                        <option value="37">37</option>
+                                        <option value="38">38</option>
+                                        <option value="39">39</option>
+                                        <option value="40">40</option>
+                                        <option value="41">41</option>
+                                        <option value="42">42</option>
+                                        <option value="43">43</option>
+                                        <option value="44">44</option>
+                                        <option value="45">45</option>
+                                        <option value="46">46</option>
+                                        <option value="47">47</option>
+                                        <option value="48">48</option>
+                                        <option value="49">49</option>
+                                        <option value="50">50</option>
+                                        <option value="51">51</option>
+                                        <option value="52">52</option>
+                                        <option value="53">53</option>
+                                        <option value="54">54</option>
+                                        <option value="55">55</option>
+                                        <option value="56">56</option>
+                                        <option value="57">57</option>
+                                        <option value="58">58</option>
+                                        <option value="59">59</option>
+                                    </select>
+                                    <script>
+                                        document.querySelector("#minute").value = "";
+                                    </script>
+                                    </div>
+                                </div>
+								
+                            </div>
+                            <div class="row ">
+                                <div class="form-group col-lg">
+                                    <button class="btn btn-danger" type="submit">ทำนายเบอร์</button>
+                                </div>
+                            </div>                        
+                        </form>	
+		
+		</div>
+<section class="mg-tpt-tag-area">
+  <div class="container-fluid">
+ <?php $show_popular_tags_title = newsup_get_option('show_popular_tags_title');
+ $select_popular_tags_mode = newsup_get_option('select_popular_tags_mode');
+ $number_of_popular_tags = newsup_get_option('number_of_popular_tags');
+ newsup_list_popular_taxonomies($select_popular_tags_mode, $show_popular_tags_title, $number_of_popular_tags); ?>
+</div>
+</section>
+ <?php }?>
+ <?php do_action('newsup_action_banner_exclusive_posts'); 
+ do_action('newsup_action_front_page_main_section_1'); ?>
+		
